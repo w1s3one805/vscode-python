0609d3d654430c7b9f91c3f533f1ec736a77782b
Karthik Nadig <kanadig@microsoft.com>
2020-11-18 19:25:10 -0800

    Point release: cherry picks, versions and change log update. (#14775)
    * Add new tryPylance banner experiments (#14759)
    
    Co-authored-by: Bill Schnurr <bschnurr@microsoft.com>
    
    * Put linter prompt behind experiment (#14731)
    
    * Add new linter prompt
    
    * Tests
    
    * Fix typos
    
    * Show linter install message only once.
    
    * Add more tests and news
    
    * Updates for point release.
    
    Co-authored-by: Jake Bailey <5341706+jakebailey@users.noreply.github.com>
    Co-authored-by: Bill Schnurr <bschnurr@microsoft.com>

--- src/client/common/experiments/groups.ts
+++ src/client/common/experiments/groups.ts
@@ -55,7 +55,9 @@ export enum DeprecatePythonPath {
 
 // Experiment to offer switch to Pylance language server
 export enum TryPylance {
-    experiment = 'tryPylance'
+    experiment = 'tryPylance',
+    jediPrompt1 = 'tryPylancePromptText1',
+    jediPrompt2 = 'tryPylancePromptText2'
 }
 
 // Experiment for the content of the tip being displayed on first extension launch:
@@ -79,4 +81,11 @@ export enum JoinMailingListPromptVariants {
 // Experiment to use a different method for normalizing code to be sent to the REPL.
 export enum SendSelectionToREPL {
     experiment = 'pythonSendEntireLineToREPL'
+}
+
+// Experiment to show a prompt asking users to install or select linter
+export enum LinterInstallationPromptVariants {
+    pylintFirst = 'pythonInstallPylintButtonFirst',
+    flake8First = 'pythonInstallFlake8ButtonFirst',
+    noPrompt = 'pythonNotDisplayLinterPrompt'
 }
